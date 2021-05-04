b66163c5228902fb42d1396666613a9c96e6c6d1
kakasilva <>
2021-05-04 00:48:06 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,250 @@
+var parar = 0;
+while (parar == 0){
+console.log("")
+console.log("Bem vindo á natura global")
+console.log("")
+console.log("nossos produtos são 100% naturais")
+console.log("")
+console.log("lista de produtos:")
+console.log("")
+console.log("Ervas medicinais")
+var erva = 100;
+console.log("")
+console.log("Essências")
+var ess = 50;
+console.log("")
+console.log("Cremes")
+var creme = 80;
+console.log("")
+var expr = prompt("oque deseja hoje?")
+
+if (expr == 'Ervas medicinais'){
+
+console.log("")
+console.log("O pacote de ervas está saindo por: R$100,00 ")
+console.log("")
+console.log("o pagamento pode ser feito por")
+console.log("")
+console.log("Dinheiro ou Cartão")
+console.log("")
+var expu = prompt("Qual será a forma de pagamento?")
+
+console.log("")
+
+switch (expu){
+
+  case 'Dinheiro' :
+  console.log("Ok, quando o motoboy for enviado a sua casa")
+  console.log("ele receberá o dinheiro")
+  console.log("")
+ for(var expf = prompt("Digite seu endereço:"); expf <= 3; expf++){console.log(expr)}
+
+  console.log("")
+   console.log("perfeito precisamos confimar")
+    console.log("se você não é um robo por segurança")
+     console.log("")
+      console.log("essa é sua id 1873")
+       console.log("")
+ var expz = prompt("confirme a ID")
+
+     do {
+  console.log("obrigado por verificar!")
+} while(expz === 1873)
+ console.log("")
+  console.log("o motoboy chegará em breve ao endereço dado")
+break
+
+case 'Cartão' :
+
+ console.log("perfeito precisamos confimar")
+    console.log("se você não é um robo por segurança")
+     console.log("")
+      console.log("essa é sua id 1873")
+       console.log("")
+ var expz = prompt("confirme a ID")
+
+   do {
+  console.log("obrigado por verificar!")
+} while(expz === 1873)
+ console.log("")
+ console.log("pelo pagamento ser no cartão precisamos cobrar a taxa de entrega que é R$20,00")
+ console.log("o valor final é de")
+ console.log(erva + 20)
+console.log(" ")
+ console.log("recisamos das seguintes informações")
+ console.log(" ")
+ var eo = prompt("numero do cartão:")
+console.log(" ")
+var eo = prompt("vencimento do cartão:")
+console.log(" ")
+var eo = prompt("cvv do cartão:")
+console.log(" ")
+var eo = prompt("senha do cartão:")
+console.log(" ")
+console.log("perfeito! o pagamento foi concluido")
+console.log(" ")
+for(var expf = prompt("Digite seu endereço:"); expf <= 3; expf++){console.log(expr)}
+console.log(" ")
+console.log("otimo o motoboy chegará em breve")
+console.log(" ")
+console.log("obrigado por comprar conosco")
+break
+}
+
+break}
+
+else if (expr == 'Essências'){
+
+console.log("")
+console.log("O pacote de Essências está saindo por: R$50,00 ")
+console.log("")
+console.log("o pagamento pode ser feito por")
+console.log("")
+console.log("Dinheiro ou Cartão")
+console.log("")
+var expu = prompt("Qual será a forma de pagamento?")
+
+console.log("")
+
+switch (expu){
+
+  case 'Dinheiro' :
+  console.log("Ok, quando o motoboy for enviado a sua casa")
+  console.log("ele receberá o dinheiro")
+  console.log("")
+ for(var expf = prompt("Digite seu endereço:"); expf <= 3; expf++){console.log(expr)}
+
+  console.log("")
+   console.log("perfeito precisamos confimar")
+    console.log("se você não é um robo por segurança")
+     console.log("")
+      console.log("essa é sua id 1873")
+       console.log("")
+ var expz = prompt("confirme a ID")
+
+     do {
+  console.log("obrigado por verificar!")
+} while(expz === 1873)
+ console.log("")
+  console.log("o motoboy chegará em breve ao endereço dado")
+break
+
+case 'Cartão' :
+
+ console.log("perfeito precisamos confimar")
+    console.log("se você não é um robo por segurança")
+     console.log("")
+      console.log("essa é sua id 1873")
+       console.log("")
+ var expz = prompt("confirme a ID")
+
+   do {
+  console.log("obrigado por verificar!")
+} while(expz === 1873)
+ console.log("")
+ console.log("pelo pagamento ser no cartão precisamos cobrar a taxa de entrega que é R$20,00")
+ console.log("o valor final é de")
+ console.log(ess + 20)
+console.log(" ")
+ console.log("recisamos das seguintes informações")
+ console.log(" ")
+ var eo = prompt("numero do cartão:")
+console.log(" ")
+var eo = prompt("vencimento do cartão:")
+console.log(" ")
+var eo = prompt("cvv do cartão:")
+console.log(" ")
+var eo = prompt("senha do cartão:")
+console.log(" ")
+console.log("perfeito! o pagamento foi concluido")
+console.log(" ")
+for(var expf = prompt("Digite seu endereço:"); expf <= 3; expf++){console.log(expr)}
+console.log(" ")
+console.log("otimo o motoboy chegará em breve")
+console.log(" ")
+console.log("obrigado por comprar conosco")
+break
+}
+
+break}
+
+else if (expr == 'Cremes'){
+
+
+console.log("")
+console.log("O pacote de Cremes está saindo por: R$80,00 ")
+console.log("")
+console.log("o pagamento pode ser feito por")
+console.log("")
+console.log("Dinheiro ou Cartão")
+console.log("")
+var expu = prompt("Qual será a forma de pagamento?")
+
+console.log("")
+
+switch (expu){
+
+  case 'Dinheiro' :
+  console.log("Ok, quando o motoboy for enviado a sua casa")
+  console.log("ele receberá o dinheiro")
+  console.log("")
+ for(var expf = prompt("Digite seu endereço:"); expf <= 3; expf++){console.log(expr)}
+
+  console.log("")
+   console.log("perfeito precisamos confimar")
+    console.log("se você não é um robo por segurança")
+     console.log("")
+      console.log("essa é sua id 1873")
+       console.log("")
+ var expz = prompt("confirme a ID")
+
+     do {
+  console.log("obrigado por verificar!")
+} while(expz === 1873)
+ console.log("")
+  console.log("o motoboy chegará em breve ao endereço dado")
+break
+
+case 'Cartão' :
+
+ console.log("perfeito precisamos confimar")
+    console.log("se você não é um robo por segurança")
+     console.log("")
+      console.log("essa é sua id 1873")
+       console.log("")
+ var expz = prompt("confirme a ID")
+
+   do {
+  console.log("obrigado por verificar!")
+} while(expz === 1873)
+ console.log("")
+ console.log("pelo pagamento ser no cartão precisamos cobrar a taxa de entrega que é R$20,00")
+ console.log("o valor final é de")
+ console.log(creme + 20)
+console.log(" ")
+ console.log("recisamos das seguintes informações")
+ console.log(" ")
+ var eo = prompt("numero do cartão:")
+console.log(" ")
+var eo = prompt("vencimento do cartão:")
+console.log(" ")
+var eo = prompt("cvv do cartão:")
+console.log(" ")
+var eo = prompt("senha do cartão:")
+console.log(" ")
+console.log("perfeito! o pagamento foi concluido")
+console.log(" ")
+for(var expf = prompt("Digite seu endereço:"); expf <= 3; expf++){console.log(expr)}
+console.log(" ")
+console.log("otimo o motoboy chegará em breve")
+console.log(" ")
+console.log("obrigado por comprar conosco")
+break
+}
+
+break}
+
+else if (expr != 'Ervas medicinais' && expr!= 'Essências' && expr != 'Cremes'){
+  console.log("tente novamente")
+}
+}
